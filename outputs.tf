370d0e91336272e9b5ab4e2496939edf51ca892d
yasp-g <jasper.h.g@icloud.com>
2023-09-06 20:38:00 +0200

    updated `certificate_arns` and `validation_options` to account for multiple certifications.

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,12 @@ output "hosted_zone_name_servers" {
   value       = aws_route53_zone.primary.name_servers
 }
 
-output "certificate_arn" {
-  description = "The ARN of the certificate"
-  value       = aws_acm_certificate.cert.arn
+output "certificate_arns" {
+  description = "The ARN for each certificate"
+  value       = [for c in aws_acm_certificate.cert : c.arn]
 }
 
 output "validation_options" {
-  description = "The domain validation options of the certificate"
-  value       = aws_acm_certificate.cert.domain_validation_options
+  description = "The domain validation options of each certificate"
+  value       = [for c in aws_acm_certificate.cert : c.domain_validation_options]
 }
